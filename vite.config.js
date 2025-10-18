71f569c21ab65d2e8fe35f6641566513a184f5ab
bkrawitz101 <bkrawitz@gmail.com>
2025-10-18 16:42:59 -0700

    up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<repository-name>/', // IMPORTANT: Replace <repository-name> with your repository name
+  base: '/Ascension-Ranch-Pilot/',
   plugins: [react()],
 })
